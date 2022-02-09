23e31fa8f98ce8186b897768a33090f0338592af
Fede Re <fede90.re@gmail.com>
2022-02-09 18:24:06 -0300

    agrego base de datos

--- routes/animales.js
+++ routes/animales.js
@@ -2,6 +2,21 @@ const express = require("express");
 const animalesController = require("../controllers/animalesControllers");
 const router = express.Router()
 
+// VER TODOS LOS ANIMALES
 router.get("/", animalesController.todos);
+
+//CREAR UN ANIMAL
+router.get("/crear", animalesController.crear);
+router.post("", animalesController.store); //use STORE porque lo usaba en la clase 21, despues cambiarlo
+
+// DETALLE DE UN ANIMAL
+router.get("/:id/", animalesController.detalle);
+
+// EDITAR UN ANIMAL
+router.get("/:id/editar", animalesController.editar);
+router.put("/:id", animalesController.update);
+
+// ELIMINAR UN PRODUCTO
+router.delete("/:id", animalesController.eliminar)
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const path = require("path");
+
 const indexController = require("../controllers/indexController");
 const router = express.Router();
 
