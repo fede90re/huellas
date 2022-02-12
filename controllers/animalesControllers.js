9c78e6cae1cf354de3f6ac3ef7c5af9a8356c116
Fede Re <fede90.re@gmail.com>
2022-02-12 18:32:33 -0300

    solucionando problema base de datos

--- controllers/animalesControllers.js
+++ controllers/animalesControllers.js
@@ -1,35 +1,38 @@
 const path = require("path");
+const fs = require("fs");
+
+//para que funcione la base de datos
+const animalesFilePath = path.join(__dirname, '../src/data/animalesDataBase.json');
+const animalesFileText = JSON.parse(fs.readFileSync(animalesFilePath, 'utf-8'));
+const animales = JSON.parse(animalesFileText); //ARRAY de ANIMALES
+
 
 const animalesController = {
 
+    todos: function (req, res) {
+        res.render("animales", { animales: animales })
+    },
+
     gatos: function (req, res) {
         let id = req.params.id;
-        res.render("gatos", { "id": id });
+        res.render("gatos", { id: id });
     },
     perros: function (req, res) {
         res.render("perros");
     },
-    todos: function (req, res) {
-        res.render("animales");
+    store: function (req, res) {
+        res.render("animales", { animales: animales });
     },
     detalleP: function (req, res) {
         let id = req.params.id;
-        res.render("detallePerros", { "id": id })
+        res.render("detallePerros", { id: id })
     },
     detalleG: function (req, res) {
         let id = req.params.id;
-        let detalle = [
-            { id: 1, nombre: "Rodolfo y Georgina" },
-            { id: 2, nombre: "Lara" },
-            { id: 3, nombre: "Enrique" },
-            { id: 4, nombre: "Loqui" },
-            { id: 5, nombre: "Peque" },
-            { id: 6, nombre: "Marce" }
-        ]
 
         //  al pasarle el segundo parámetro puedo utilizar información en la vista con ejs, 
         //  se pueden compartir más de una variable. 
-        res.render("detalleGatos", { "id": id, "detalle": detalle });
+        res.render("detalleGatos", { "id": id });
     },
 
     editar: function () { },
