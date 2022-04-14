e34a54ea57cccaae034e3c8bba0a7eb4e8b7e5fa
Fede Re <fede90.re@gmail.com>
2022-04-14 20:10:35 -0300

    agrego funcionalidades de actualizar y eliminar un animal en services

--- src/controllers/animalesControllers.js
+++ src/controllers/animalesControllers.js
@@ -16,74 +16,33 @@ const animalesController = {
     },
 
     guardar: function (req, res) {
-        let animalNuevo = {
-            id: Date.now(),
-            // con la destructuración "..." es lo mismo que tomar todas las propiedades del body
-            // y aplicarlas al objeto, y usar los valores del body en ese objeto
-            ...req.body
-        };
-        animalesService.animales.push(animalNuevo);
-        animalesService.guardarAnimal();
-
+        animalesService.crearUno(req.body)
         res.redirect("/animales");
     },
 
     detalle: function (req, res) {
         const id = req.params.id;
-
-        const animal = animalesService.findOne(id);
-
+        const animal = animalesService.EncontrarUno(id);
         res.render("detalle", { animal });
     },
 
-
-    // Utilizo el find para encontrar el primer animal que coincida con el id que se busca.
-    // Así solamente necesito pasarle a la vista la página a renderizar y la variable que declaro.
     editar: function (req, res) {
         const id = req.params.id;
-        const animal = animales.find((animals) => {
-            return animals.id == id;
-        });
+        const animal = animalesService.EncontrarUno(id);
         res.render("editarAnimal", {
-            animal
+            animal,
         })
-        res.send(animal); //¿Va todavía?
     },
 
     update: function (req, res) {
-
         const id = req.params.id;
-
-        //con el findIndex busco el índice del array donde está ese animal
-        const indice = animales.findIndex((animals) => {
-            return animals.id == id;
-        });
-
-        //modifico el animal
-        const actualizarAnimal = {
-            ...req.body,
-        };
-
-        //piso el elemento del array con ese animal que modifique 
-        animales[indice] = actualizarAnimal;
-
-        animalesService.guardarAnimal();
-
-        res.redirect("/animales/" + id)
+        animalesService.actiualizarUno(id, req.body);
+        res.redirect("/animales/" + id);
     },
 
     eliminar: function (req, res) {
-
         const id = req.params.id;
-
-        const indice = animales.findIndex((animals) => {
-            return animals.id == id;
-        });
-
-        //el SPLICE me borra un animal del array en el INDICE que le indico arriba
-        animales.splice(indice, 1);
-        animalesService.guardarAnimal();
-
+        animalesService.eliminarUno(id);
         res.redirect("/animales");
     }
 
--- src/services/animales.js
+++ src/services/animales.js
@@ -15,11 +15,47 @@ module.exports = {
     animales,
     guardarAnimal,
 
-    findOne(id) {
+    EncontrarUno(id) {
         const animal = animales.find((animals) => {
             return animals.id == id;
         });
         return animal;
     },
 
+    crearUno(body) {
+        let animalNuevo = {
+            id: Date.now(),
+            // con la destructuración "..." es lo mismo que tomar todas las propiedades del body
+            // y aplicarlas al objeto, y usar los valores del body en ese objeto
+            ...body
+        };
+        animales.push(animalNuevo);
+        guardarAnimal();
+    },
+
+    actiualizarUno(id, body) {
+        //con el findIndex busco el índice del array donde está ese animal
+        const indice = animales.findIndex((animals) => {
+            return animals.id == id;
+        });
+
+        const actualizarAnimal = {
+            id: animales[indice].id,
+            ...body,
+        };
+
+        animales[indice] = actualizarAnimal;
+
+        guardarAnimal();
+    },
+
+    eliminarUno(id) {
+        const indice = animales.findIndex((animals) => {
+            return animals.id == id;
+        });
+
+        //el SPLICE me borra un animal del array en el INDICE que le indico arriba
+        animales.splice(indice, 1);
+        guardarAnimal();
+    },
 }
